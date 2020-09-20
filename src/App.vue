e09dd7a55b9bf586ae40b0f11e15f13744dbe0a7
hongtao <343275891@qq.com>
2020-09-20 18:40:45 +0800

    第二次修改

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        app
+        <!--<input type="button" value="点我试试"
+        @click="sayHi">-->
+<!--        头部组件-->
+    <top></top>
     </div>
 </template>
 
 <script>
+    import top from 'src/components/01.top'
     export default {
-        name: "App"
+        //注册组件
+        components:{
+          top//top:top
+        },
+        methods:{
+            sayHi(){
+                alert("我出现了！！！")
+            }
+        }
     }
 </script>
 
---
+++ src/components/01.top.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <img src="src/assets/01top.png">
+        <input type="text">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "01.top.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
